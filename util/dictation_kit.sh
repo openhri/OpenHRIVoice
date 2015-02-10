3cb040e2ca4f4eb965b09fa5dad213e2b2767b5f
test <test@ubuntu1204-test.(none)>
2015-02-10 11:41:03 +0900

    change shellscript for dictation-kit

--- util/dictation_kit.sh
+++ util/dictation_kit.sh
@@ -2,9 +2,9 @@
 
 target_name="julius-runkit_with_dictation-kit"
 target_copyright="./lice_dir/julius_copyright_utf8.txt"
-target_url="http://en.sourceforge.jp/frs/redir.php?m=jaist&f=%2Fjulius%2F60416%2Fdictation-kit-v4.3.1-linux.tgz"
+target_url="http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fjulius%2F59050%2Fdictation-kit-v4.2.3.tar.gz"
 target_option1="-O"
-target_option2="dictation-kit-v4.3.1-linux.tgz"
+target_option2="dictation-kit-v4.2.3.tar.gz"
 target_installPath="/usr/local/share"
 
 . ./func_setup.sh
@@ -21,13 +21,13 @@ echo "${target_url}"
 func_DL $target_name "${target_url}" "${target_option1}" "${target_option2}" 
 
 # Set archive name in target_file .
-target_file="dictation-kit-v4.3.1-linux.tgz"
+target_file="dictation-kit-v4.2.3-linux.tgz"
 
 # Extrac archive.
 func_extracFile $target_file $target_installPath
 
 # Set to Symbolic link.
-/usr/bin/sudo /bin/ln -s /usr/local/share/dictation-kit-v4.3.1-linux /usr/local/share/julius-runkit
+/usr/bin/sudo /bin/ln -s /usr/local/share/dictation-kit-v4.2.3 /usr/local/share/julius-runkit
 
 }
 
